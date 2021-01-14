b4e05ac3e29e9dc184cc1db045e64d62a721cbd1
alatif <alatif@uw.edu>
2021-01-13 22:31:18 -0800

    changed api to pass less data on network for faster loading

--- index.js
+++ index.js
@@ -16,12 +16,70 @@ export const QUARTER_GAP = 500;
 const DATA = "https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/e/2PACX-1vQh1nE1K_4KeNgcCpM5Y0OFEG5hyweGzNP4d0SZBu7VgkZeNjeESvWWAK_CMIlDXqiybLjZkTw371I0/pub?gid=0&single=true&output=csv";
 
 const Http = new XMLHttpRequest();
-const url = 'https://nameless-atoll-70309.herokuapp.com/api/getTaNodes';
+const url = 'https://nameless-atoll-70309.herokuapp.com/api/getTas';
 
 window.onload = function() {
     Http.onreadystatechange = function() {
         if (this.readyState==4 && this.status==200) {
-            loadGraphFromJson(JSON.parse(Http.response));
+            let spreadsheet_data = JSON.parse(Http.response);
+            var results = spreadsheet_data["values"];
+            function addEdge(parent, child, relation) {
+                if ((!(parent in tas)) && parent !== "") {
+                    taNames.push(parent);
+                    tas[parent] = { "id" : parent,
+                        "parent142" : "",
+                        "parent143" : "",
+                        "parent143x" : "",
+                        "num_142_quarters" : 0,
+                        "num_143_quarters" : 0,
+                        "num_143x_quarters" : 0,
+                        "num_14x_quarters" : 0,
+                        "cohort" :  "05au",
+                        "img" : '',
+                        "children" : [],
+                        "kudos" : "" ,
+                        "nicknames" : ""
+                    };
+                }
+                if (parent !== "") {
+                    links.push({"source" : parent, "target" : child, "type": relation, "info_src": tas[parent], "info_child" :tas[child]});
+                    tas[parent]["children"].push(tas[child]);
+                }
+            }
+
+            let tas = {};
+            for (var i = 1; i < results.length; i++) {
+                let taName, ta142, ta143, ta143x, num142, num143, num143x, num14x, cohort, kudos, nicknames, img;
+                [taName, ta142, ta143, ta143x, num142, num143, num143x, num14x, cohort, kudos, nicknames, img] = results[i];
+                tas[taName] = { "id" : taName,
+                                "parent142" : ta142 ? ta142 : '',
+                                "parent143" : ta143 ? ta143 : '',
+                                "parent143x" : ta143x ? ta143x : '',
+                                "num_142_quarters" : num142 ? num142 : 0,
+                                "num_143_quarters" : num143 ? num143 : 0,
+                                "num_143x_quarters" : num143x ? num143x : 0,
+                                "num_14x_quarters" : num14x ? num14x : 0,
+                                "cohort" : cohort ? cohort : 0,
+                                "img" : img,
+                                "children" : [],
+                                "kudos" : kudos ? kudos : '',
+                                "nicknames" : nicknames ? nicknames : ''
+                            }
+            }
+            let links = [];
+
+            let taNames = Array.from(Object.keys(tas));
+            taNames.forEach(function(taName) {
+                addEdge(tas[taName]["parent142"], taName, "parent142")
+                addEdge(tas[taName]["parent143"], taName, "parent143")
+                addEdge(tas[taName]["parent143x"], taName, "parent143x")
+            });
+            let nodes = [];
+            taNames.forEach(function(taName) {
+                nodes.push(tas[taName]);
+            });
+
+            loadGraphFromJson({"nodes": nodes, "links": links});
             document.getElementById("testing").classList.remove("loader");
         }
     }
